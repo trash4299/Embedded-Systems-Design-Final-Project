f28c6a6265052179331cde150e8f6ab830831357
Vincent Lobo <vincelobo4299@gmail.com>
2020-04-15 01:35:01 -0500

    Finished convertSampless, but the UART is wrong
    The UART is not scanning in the numbers right even though the c file for this project was provided by our teacher

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -8,14 +8,14 @@ SHELL = cmd.exe
 %.obj: ../%.asm $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccs810/ccsv8/ccs_base/msp430/include" --include_path="C:/Users/vince/Documents/Current/Embedded/vpl8347_EEEE-420/vpl8347_Lab11" --include_path="C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccs810/ccsv8/ccs_base/msp430/include" --include_path="C:/Users/vince/Documents/Current/Embedded/vpl8347_EEEE-420/vpl8347_Lab11" --include_path="C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --symdebug:dwarf_version=3 --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccs810/ccsv8/ccs_base/msp430/include" --include_path="C:/Users/vince/Documents/Current/Embedded/vpl8347_EEEE-420/vpl8347_Lab11" --include_path="C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccs810/ccsv8/ccs_base/msp430/include" --include_path="C:/Users/vince/Documents/Current/Embedded/vpl8347_EEEE-420/vpl8347_Lab11" --include_path="C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include" --advice:power=all --define=__MSP430G2553__ -g --symdebug:dwarf_version=3 --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
--- vpl8347_Lab11_a1.asm
+++ vpl8347_Lab11_a1.asm
@@ -1,16 +1,28 @@
 
-	.cdecls C,LIST,"msp430g2553.h"       ; Include device header file
+	.cdecls C,LIST,"msp430g2553.h"
 
-LED0		.equ	0x01
+;Constants
+LED0		.equ	0x01	;for a single LED on
 LED1		.equ	0x10
 LED2		.equ	0x20
 LED3		.equ	0x40
 LED4		.equ	0x80
-LED5		.equ	0xE8
-LED6		.equ	0xD8
-LED7		.equ	0xB8
+LED5		.equ	0xe8
+LED6		.equ	0xd8
+LED7		.equ	0xb8
 LED8		.equ	0x78
 
+LEDs0		.equ	0x00	;for multiple LEDs on
+LEDs1		.equ	0x10
+LEDs2		.equ	0x30
+LEDs3		.equ	0x70
+LEDs4		.equ	0xf0
+LEDs5		.equ	0xe8
+LEDs6		.equ	0xc8
+LEDs7		.equ	0x88
+LEDs8		.equ	0x08
+
+;Global functions
 		.global setup
 		.global waitForCenter
 		.global waitForUpDown
@@ -18,12 +30,17 @@ LED8		.equ	0x78
 		.global getSamples
 		.global convertSamples
 		.global displaySamples
+		.global UART_samples
 
+;Variables
 		.data
 		.bss	meas_base, 10
 		.bss	meas_latest, 10
 		.bss	sensor_status, 1
 		.bss	SWdelay, 1
+		.bss	conArray, 20	;num of LEDs to light up (0-8) for each sample
+
+;Subroutines
 		.text
 setup:
 		bis.b	#0xf9, &P1DIR	; set up P1 as outputs
@@ -33,8 +50,6 @@ setup:
 		mov.b	#0x1a, SWdelay
 		call	#meas_base_val
 		mov.b	#0xdf, SWdelay
-		clr		r13
-		clr 	r14
 		ret
 
 meas_setup:
@@ -143,17 +158,91 @@ meas3		call	#meas_latest_val
 			cmp		meas_latest(r14), meas_base(r14)
 			jge		endwait3b
 			jmp		meas3
-endwait3a	bic		#0, r13
+endwait3a	clr		P1OUT
+			bis		#2, r13
 			ret
-endwait3b	bis		#2, r13
+endwait3b	clr		P1OUT
+;bic		#2, r13			;this bit will be set because of mov in waitForUpDown
 			ret
 
 convertSamples:
+			mov		#0, r10		; loop counter and conArray index
+			mov		#0, r11		; UART_samples index
+			cmp		#2, r13		; checks lin or log
+			jge		conlin
+conlog		mov.b	#1, conArray(r10)
+			bic		#0x0003, UART_samples(r11)
 
+			cmp		#0x00000000, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x00000001, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x00000010, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x00000100, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x00001000, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x00010000, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x00100000, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x0100000, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			;cmp		#0x1000000, UART_samples(r11)		;it should never reach here
+next1		add		#4, r11
+			inc		r10
+			cmp		#0x14, r10
+			jne		conlog
+			ret
+conlin		mov.b	#1, conArray(r10)
+			bic		#0x007f, UART_samples(r11)
+
+			cmp		#0x0000, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x0080, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x0100, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x0180, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x0200, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x0280, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			cmp		#0x0300, UART_samples(r11)
+			jeq		next2
+			inc.b	conArray(r10)
+			;cmp		#0x0380, UART_samples(r11)		;it should never reach here
+next2		add		#4, r11
+			inc		r10
+			cmp		#0x14, r10
+			jne		conlin
 			ret
 
 displaySamples:
+			mov		#0, r10		; loop counter and conArray index
+			bic		#2, r13
+			cmp		#1, r13		; checks lin or log
+			jeq		five
+two
 
+			ret
+five
 			ret
 
 meas_base_val:
@@ -161,33 +250,33 @@ meas_base_val:
 			mov.b	#0x00, R6	; initialize R6 to the base of meas_base
 meas_base_again
 			call 	#meas_setup
-			bis 	#MC_2 + TACLR, &TA0CTL 	; Clear TAR and start TA0 in continuous mode
-			call	#SWtimer			;provide the accumulation period;could use instead ACLK fed from VLO
-			xor		#CCIS0, &TA0CCTL1	;capture trigger by toggeling CCIS0
+			bis 	#MC_2 + TACLR, &TA0CTL
+			call	#SWtimer				;provide the accumulation period.could use ACLK fed from VLO instead
+			xor		#CCIS0, &TA0CCTL1		;capture trigger by toggeling CCIS0
 			mov		TA0CCR1, meas_base(R6)
-			bic 	#MC1+MC0, &TA0CTL 	; Stop TA
+			bic 	#MC1+MC0, &TA0CTL
 			sub 	#0x20, meas_base(R6)	; Adjust this baseline
-			bic.b 	R5,&P2SEL2		; Stop the oscillation on the latest. pin
-			rla.b	R5				; Prepare next x
-			add.b	#0x02, R6		; Prepare the next index into the array
-			cmp.b	#0x40, R5		; Check if done with all five sensors
-			jne		meas_base_again	;
-			ret						;
+			bic.b 	R5,&P2SEL2				; Stop the oscillation on the latest. pin
+			rla.b	R5						; Prepare next x
+			add.b	#0x02, R6				; Prepare the next index into the array
+			cmp.b	#0x40, R5				; Check if done with all five sensors
+			jne		meas_base_again
+			ret
 
 meas_latest_val:
-			mov.b	#0x02, R5	; initialize R5 to point to P2.1
+			mov.b	#0x02, R5		; initialize R5 to point to P2.1
 			mov.b	#0x00, R6		; initialize R6 to the base of meas_base
 meas_latest_again
-			call 	#meas_setup	;
-			bis 	#MC_2 + TACLR, &TA0CTL 	; Continuous, Clear TAR
+			call 	#meas_setup
+			bis 	#MC_2 + TACLR, &TA0CTL
 			call	#SWtimer
-			xor		#CCIS0, &TA0CCTL1	; Trigger SW capture
+			xor		#CCIS0, &TA0CCTL1			; Trigger SW capture
 			mov 	TA0CCR1, meas_latest(R6)	; Save captured value in array
-			bic 	#MC1+MC0, &TA0CTL 	; Stop timer
-			bic.b 	R5,&P2SEL2		; Stop the oscillation on the latest. pin
-			rla.b	R5				; Prepare next x
-			add.b	#0x02, R6		; Prepare the next index into the array
-			cmp.b	#0x40, R5		; Check if done with all five sensors
+			bic 	#MC1+MC0, &TA0CTL
+			bic.b 	R5,&P2SEL2					; Stop the oscillation on the latest pin
+			rla.b	R5							; Prepare next x
+			add.b	#0x02, R6					; Prepare the next index into the array
+			cmp.b	#0x40, R5					; Check if done with all five sensors
 			jne		meas_latest_again
 			ret
 
@@ -197,11 +286,11 @@ det_sensor:	clr.b	sensor_status
 CheckNextSensor
 			cmp		meas_latest(R6), meas_base(R6)
 			jl		NotThisSensor
-			bis.b	R5, sensor_status	; Update sensor_status
+			bis.b	R5, sensor_status
 NotThisSensor
-			rla.b	R5				; Prepare next x
-			add.b	#0x02, R6		; Prepare the next index into the array
-			cmp.b	#0x40, R5		; Check if done with all five sensors
+			rla.b	R5					; Prepare next x
+			add.b	#0x02, R6			; Prepare the next index into the array
+			cmp.b	#0x40, R5			; Check if done with all five sensors
 			jne		CheckNextSensor
 			ret
 
@@ -230,7 +319,7 @@ checknon	mov 	#0x00, P1OUT
 			ret
 
 SWtimer:
-			mov.b	SWdelay, r8				; The total SW delay count = SWdelay * SWdelay
+			mov.b	SWdelay, r8			; The total SW delay count = SWdelay * SWdelay
 Reloadr7	mov.b	SWdelay, r7
 ISr70		dec		r7
 			jnz		ISr70
@@ -238,4 +327,4 @@ ISr70		dec		r7
 			jnz		Reloadr7
 			ret
 
-	.end
+.end
